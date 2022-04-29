f0c64693bb648acacbedf7caf853fe7c6bb0e20a
victorHL99 <victor.lessa@estudante.ufla.br>
2022-04-29 18:53:42 -0300

    Função post participants iniciada

--- back-end/app.js
+++ back-end/app.js
@@ -6,4 +6,14 @@ import {MongoClient} from "mongodb";
 const app = express();
 app.use(cors());
 app.use(json());
-app.listen(5000, () => {console.log(chalk.red.bold("Servidor iniciado na porta 5000"))});
+app.listen(5000, () => {
+    console.log(chalk.red.bold("Servidor iniciado na porta 5000"))
+});
+
+
+//Post /participants 
+app.post("/participants", (req, res) => {
+    const {name} = req.body;
+    console.log(name);
+    res.send("ok");
+})
