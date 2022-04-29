57e2b4783a89347a2705b3b5d9bf7b866eb7d2c0
victorHL99 <victor.lessa@estudante.ufla.br>
2022-04-29 19:11:33 -0300

    Function get/participants ok

--- back-end/app.js
+++ back-end/app.js
@@ -10,10 +10,23 @@ app.listen(5000, () => {
     console.log(chalk.red.bold("Servidor iniciado na porta 5000"))
 });
 
+const participants = [];
 
 //Post /participants 
 app.post("/participants", (req, res) => {
     const {name} = req.body;
-    console.log(name);
-    res.send("ok");
+    //Validar: (caso algum erro seja encontrado, retornar status 422)
+    //name deve ser strings não vazio
+    if(!name) {
+        res.sendStatus(422);
+    } else {
+        console.log(name);
+        participants.push(name);
+        res.send("ok");
+
+    }
+})
+
+app.get("/participants", (req, res) => {
+    res.send(participants)
 })
